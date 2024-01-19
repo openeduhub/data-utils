272dca08fdb89de0e6d2dac94c40524c2ef2eecf
jokatzke <jokatzke@fastmail.com>
2024-01-19 14:34:15 +0100

    feat: implement remaining filters & fix issues from tests

--- data_utils/filters.py
+++ data_utils/filters.py
@@ -1,74 +1,83 @@
-from collections.abc import Callable
+import operator as op
+from collections.abc import Callable, Collection, Sequence
+from functools import partial
+from typing import TypeVar
 
-from data_utils.utils import Nested_Dict, get_in, get_terminal_in
-
+from data_utils.utils import Basic_Value, Nested_Dict, Terminal_Value, get_terminal_in
 
 Filter = Callable[[Nested_Dict], bool]
 
+T = TypeVar("T")
+
+
+def get_predicate_on_terminal(
+    simple_predicate: Callable[[Basic_Value], bool],
+    list_semantics: Callable[[Collection[bool]], bool],
+) -> Callable[[Terminal_Value], bool]:
+    def fun(comp_value: Terminal_Value) -> bool:
+        if isinstance(comp_value, list):
+            return list_semantics([fun(sub_value) for sub_value in comp_value])
+
+        return simple_predicate(comp_value)
+
+    return fun
+
+
+def get_filter(
+    predicate_fun: Callable[[Terminal_Value], bool], key_seq: Sequence[str]
+) -> Callable[[Nested_Dict], bool]:
+    def fun(entry: Nested_Dict) -> bool:
+        value = get_terminal_in(entry, key_seq, catch_errors=(KeyError, TypeError))
+        return predicate_fun(value)
+
+    return fun
+
+
+def get_filter_with_basic_predicate(
+    predicate_fun: Callable[[Basic_Value], bool],
+    key_seq: Sequence[str],
+    list_semantics: Callable[[Collection[bool]], bool],
+) -> Callable[[Nested_Dict], bool]:
+    return get_filter(get_predicate_on_terminal(predicate_fun, list_semantics), key_seq)
+
 
 def kibana_basic_filter(entry: Nested_Dict) -> bool:
-    must_equals = [
-        ("nodeRef.storeRef.protocol", "workspace"),
-        ("type", "ccm:io"),
-        ("properties.cm:edu_metadataset", "mds_oeh"),
+    must_filters = [
+        get_filter(
+            get_predicate_on_terminal(partial(op.eq, value), any), field.split(".")
+        )
+        for field, value in [
+            ("nodeRef.storeRef.protocol", "workspace"),
+            ("type", "ccm:io"),
+            ("properties.cm:edu_metadataset", "mds_oeh"),
+        ]
     ]
-    must_not_contain = [("aspects", "ccm:collection_io_reference")]
-
-    must_holds = all(
-        get_in(entry, key_seq.split("."), catch_errors=(KeyError, TypeError)) == value
-        for key_seq, value in must_equals
+    must_filters.append(
+        get_filter(
+            get_predicate_on_terminal(
+                partial(op.ne, "ccm:collection_io_reference"), all
+            ),
+            ["aspects"],
+        )
     )
 
-    lists = [
-        get_in(entry, key_seq.split("."), catch_errors=(KeyError, TypeError))
-        for key_seq, _ in must_not_contain
-    ]
-
-    if any(not isinstance(res_list, list) for res_list in lists):
-        return False
-
-    must_not_holds = all(
-        value not in res_list
-        for (
-            _,
-            value,
-        ), res_list in zip(must_not_contain, lists)
-    )
-
-    return must_holds and must_not_holds
+    return all(fun(entry) for fun in must_filters)
 
 
 def kibana_publicly_visible(entry: Nested_Dict) -> bool:
-    should_equals = [
-        ("permissions.Read", "GROUP_EVERYONE"),
-        ("collections.permissions.Read", "GROUP_EVERYONE"),
+    should_filters = [
+        get_filter_with_basic_predicate(partial(op.eq, value), field.split("."), any)
+        for field, value in [
+            ("permissions.Read", "GROUP_EVERYONE"),
+            ("collections.permissions.Read", "GROUP_EVERYONE"),
+        ]
     ]
 
-    return any(
-        value in get_in(entry, key_seq.split(".")) for key_seq, value in should_equals
-    )
+    return any(fun(entry) for fun in should_filters)
 
 
-def kibana_redaktionsbuffet(entry: Nested_Dict) -> bool:
-    try:
-        val: list[str] | None = get_in(
-            entry,
-            "collections.properties.cm:title".split("."),
-            catch_errors=(KeyError,),
-        )  # type: ignore
-    except TypeError:
-        print(
-            get_in(
-                entry,
-                "collections.properties".split("."),
-                catch_errors=tuple(),
-            )
-        )
-        return False
-
-    print(entry == val)
-
-    if not val:
-        return False
-
-    return any(title == "Redaktionsbuffet" for title in val)
+kibana_redaktionsbuffet = get_filter_with_basic_predicate(
+    predicate_fun=partial(op.eq, "Redaktionsbuffet"),
+    key_seq="collections.properties.cm:title".split("."),
+    list_semantics=any,
+)
--- data_utils/utils.py
+++ data_utils/utils.py
@@ -4,17 +4,39 @@ from collections.abc import Iterable, Sequence
 from typing import Any
 
 Basic_Value = str | int | float | None
-Terminal_Value = Basic_Value | list[Basic_Value]
+Terminal_Value = Basic_Value | list["Terminal_Value"]
 
 
 Nested_Dict = dict[str, Any]
-Query_Result = Terminal_Value | Nested_Dict | list[Nested_Dict]
+Query_Result = Terminal_Value | Nested_Dict | list["Query_Result"]
+
+
+def _nested_get_in(
+    nested_dict: Query_Result,
+    keys: Sequence[str],
+    catch_errors: Iterable[type[Exception]] = tuple(),
+) -> Query_Result:
+    if not keys:
+        return nested_dict
+
+    key = keys[0]
+    keys = keys[1:]
+
+    try:
+        val = nested_dict[key]  # type: ignore
+    except catch_errors as e:
+        return None
+
+    if isinstance(val, list):
+        return [_nested_get_in(sub_val, keys, catch_errors) for sub_val in val]
+
+    return _nested_get_in(val, keys, catch_errors)
 
 
 def get_in(
     nested_dict: Nested_Dict,
     keys: Sequence[str],
-    catch_errors: Iterable[type[Exception]] = tuple(),
+    catch_errors: tuple[type[Exception], ...] = tuple(),
 ) -> Query_Result:
     """
     Recursively access a nested dictionary.
@@ -25,49 +47,23 @@ def get_in(
       Catch TypeErrors to return None when a nested dictionary ends
         before all keys have been used up.
     """
-    nested_dict_iter: Nested_Dict | Terminal_Value = nested_dict
-    while True:
-        if not keys:
-            return nested_dict_iter
+    return _nested_get_in(nested_dict, keys, catch_errors)
 
-        key = keys[0]
-        keys = keys[1:]
 
-        try:
-            # type errors are OK here. errors will be caught below
-            val = nested_dict_iter[key]  # type: ignore
+def _to_terminal(result: Query_Result) -> Terminal_Value:
+    if isinstance(result, dict):
+        return None
 
-            # check if the value is a list of dictionaries.
-            # if so, we need to iterate over this list
-            if isinstance(val, list):
-                if len(val) == 0:
-                    if keys:
-                        raise TypeError()
+    if isinstance(result, list):
+        return [_to_terminal(x) for x in result]
 
-                    return []
-
-                sub_val = val[0]
-                if isinstance(sub_val, dict):
-                    # this returns either a list of terminal values
-                    # or a list of nested dicts
-                    return [get_in(sub_val, keys_iter) for sub_val in val]  # type: ignore
-
-            nested_dict_iter = val  # type: ignore
-
-        except catch_errors as e:
-            return None
+    return result
 
 
 def get_terminal_in(
     nested_dict: Nested_Dict,
     keys: Sequence[str],
-    catch_errors: Iterable[type[Exception]] = (KeyError, TypeError),
+    catch_errors: tuple[type[Exception], ...] = (KeyError, TypeError),
 ) -> Terminal_Value:
-    val = get_in(nested_dict, keys, catch_errors)
-    if isinstance(val, dict):
-        return None
-    if isinstance(val, list):
-        if len(val) > 0 and isinstance(val[0], dict):
-            return None
-
-    return val  # type: ignore
+    """Like get_in, but replace non-terminal results with None."""
+    return _to_terminal(get_in(nested_dict, keys, catch_errors))
