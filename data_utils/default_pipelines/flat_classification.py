f2995703745879d41f541d639a166b6bd034c479
jokatzke <jokatzke@fastmail.com>
2024-02-22 14:51:13 +0100

    refactor: move very basic pipeline logic to new submodule

--- data_utils/default_pipelines/flat_classification.py
+++ data_utils/default_pipelines/flat_classification.py
@@ -1,19 +1,14 @@
 from __future__ import annotations
 
 import operator as op
-from collections.abc import Collection, Sequence
+from collections.abc import Collection
 from pathlib import Path
-from typing import Optional
 
 import data_utils.defaults as defaults
-import data_utils.fetch as fetch
 import data_utils.filters as filt
-import data_utils.transform as trans
-import numpy as np
-import pandas as pd
 from data_utils.data import Basic_Value, Basic_Value_Not_None
-from data_utils.default_pipelines.data import Data, Target_Data
-from data_utils.defaults import Fields
+from data_utils.default_pipelines import basic
+from data_utils.default_pipelines.data import Data
 
 
 def generate_data(
@@ -65,106 +60,14 @@ def generate_data(
             filt.existing_text_filter,
         } | set(filters)
 
-    df = _get_basic_df(
+    return basic.generate_data(
         json_file=json_file,
         target_fields=target_fields,
+        uri_label_fields=uri_label_fields,
+        skos_urls=skos_urls,
+        skip_labels=skip_labels,
+        filters=filters,
         dropped_values=dropped_values,
         remapped_values=remapped_values,
-        filters=filters,
         **kwargs,
-    )
-
-    # get data for targets
-    target_data = {
-        target: _values_to_target_data(
-            df,
-            field=target,
-            skos_url=skos_urls.get(target, None),
-            uri_label_field=uri_label_fields.get(target, ("prefLabel", "de")),
-            skip_labels=skip_labels,
-        )
-        for target in target_fields
-    }
-
-    # concatenate title and description
-    raw_texts = df.apply(
-        lambda x: (x[Fields.TITLE.value] or "")
-        + "\n"
-        + (x[Fields.DESCRIPTION.value] or ""),
-        axis=1,
-    )
-
-    # determine whether documents belong to the redaktionsbuffet
-    redaktion_series = df[Fields.COLLECTIONS_TITLE.value].apply(
-        lambda x: x is not None and "Redaktionsbuffet" in x
-    )
-
-    return Data(
-        raw_texts=raw_texts.to_numpy(),
-        ids=df[Fields.ID.value].to_numpy(),
-        editor_arr=redaktion_series.to_numpy(dtype=bool),
-        target_data=target_data,
-    )
-
-
-def _get_basic_df(
-    json_file: Path, target_fields: Collection[str], **kwargs
-) -> pd.DataFrame:
-    df = fetch.df_from_json_file(
-        json_file,
-        columns={
-            Fields.TITLE.value,
-            Fields.DESCRIPTION.value,
-            Fields.ID.value,
-            Fields.COLLECTIONS_TITLE.value,
-            Fields.LANGUAGE.value,
-            Fields.TEST_DATA.value,
-        }
-        | set(target_fields),
-        **kwargs,
-    )
-
-    # unnest description field
-    df[Fields.DESCRIPTION.value] = df[Fields.DESCRIPTION.value].apply(
-        lambda x: x[0] if x is not None else None
-    )
-
-    return df
-
-
-def _values_to_target_data(
-    df: pd.DataFrame,
-    field: str,
-    skos_url: Optional[str],
-    uri_label_field: Sequence[str],
-    skip_labels: bool,
-) -> Target_Data:
-    values = df[field]
-
-    # transform the entries into boolean arrays
-    arr, uris = trans.as_boolean_array(values)
-
-    # get readable labels for the targets, if available
-    if not skip_labels:
-        if skos_url is not None:
-            labels: list[str | None] = fetch.labels_from_skos(
-                uris, url=skos_url, label_seq=uri_label_field, multi_value=False
-            )  # type: ignore
-        else:
-            labels = fetch.labels_from_uris(
-                uris=uris, label_seq=uri_label_field, multi_value=False
-            )  # type: ignore
-    else:
-        labels = [None for _ in uris]
-
-    return Target_Data(
-        arr=arr,
-        uris=np.array(uris),
-        labels=np.array(labels),
-        in_test_set=np.array(
-            [
-                test_ids is not None and field in test_ids
-                for test_ids in df[Fields.TEST_DATA.value]
-            ]
-        ),
     )
